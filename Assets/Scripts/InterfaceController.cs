b11f0d897775c1c1c6c0bf46035e799ceccbdfb2
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-09-25 16:59:07 +0200

    Refactored scenes' object names and organized

---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterfaceController : MonoBehaviour {
+
+	public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
